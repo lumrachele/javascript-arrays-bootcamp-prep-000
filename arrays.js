fcf9ad016fde0609cdcc2af39765d6240abf3aad
Rachel Lum <rlum894@gmail.com>
2018-09-28 22:10:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,7 @@ function destructivelyAddElementToBeginningOfArray (array, element){
   
 function addElementToEndOfArray (array, element){
   var newArray = [...array, `${element}`]
+  return newArray
 }
 
 function destructivelyAddElementToEndOfArray(array, element){

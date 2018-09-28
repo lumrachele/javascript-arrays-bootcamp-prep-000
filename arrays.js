74eee395e9367c7ea5f2791ca294e0d5fc22bf94
Rachel Lum <rlum894@gmail.com>
2018-09-28 22:18:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function accessElementInArray (array, index) {
-  array[`${index}`]
+  return array[`${index}`]
 }
   
 function destructivelyRemoveElementFromBeginningOfArray (array) {
